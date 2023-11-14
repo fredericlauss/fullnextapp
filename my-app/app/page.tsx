3a956d9a9c29031cf527581d35854d08c6eb9be2
frederic <flausson@normandiewebschool.fr>
2023-11-14 17:35:11 +0100

    good practices docker

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,39 +1,75 @@
+"use client"
 import { Items, itemsColumns } from "./items/items-columns"
 import { ItemsDataTable } from "./items/items-data-table"
 import { Rentals, rentalsColumns } from "./rentals/rentals-columns"
 import { RentalsDataTable } from "./rentals/rentals-data-table"
 import { AddItem } from "@/components/addItem"
-import { AddRental } from "@/components/addRental"
 import { Toaster } from "@/components/ui/toaster"
+import { useEffect, useState } from "react"
 
 
-async function getItems(): Promise<Items[]> {
-    const res = await fetch('http://my-api:5000/api/v1/items', {
+// async function getRentas(): Promise<Rentals[]> {
+//   const res = await fetch('http://my-api:5000/api/v1/rentals', {
+//       next: {
+//         revalidate: 0
+//       }
+//     })
+//     return res.json();
+// }
+
+
+export default function Home() {
+  const [items, setItems] = useState<Items[]>([]);
+  const [rentals, setRentals] = useState<Rentals[]>([]);
+  // const rentals = await getRentas();
+
+  const getItems = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/api/v1/items', {
         next: {
           revalidate: 0
         }
       })
-      return res.json();
-}
-
-async function getRentas(): Promise<Rentals[]> {
-  const res = await fetch('http://my-api:5000/api/v1/rentals', {
-      next: {
-        revalidate: 0
+      if (res.ok) {
+        const data = await res.json();
+        const items = data;
+        if (items) {
+          setItems(items);
+        }
       }
-    })
-    return res.json();
-}
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  const getRentals = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/api/v1/rentals', {
+        next: {
+          revalidate: 0
+        }
+      })
+      if (res.ok) {
+        const data = await res.json();
+        const rentals = data;
+        if (rentals) {
+          setRentals(rentals);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-
-export default async function Home() {
-  const items = await getItems();
-  const rentals = await getRentas();
+  useEffect(() => {
+    getItems();
+    getRentals();
+  }, [items, rentals]);
+  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h2>Items</h2>
+        <h2>Items sdfsdf</h2>
         <AddItem />
         <ItemsDataTable columns={itemsColumns} data={items} />
         <h2>rentals</h2>
