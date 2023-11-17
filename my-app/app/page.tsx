b47fc0f7ea1493f228d3b5813118a0e70ba56d37
frederic <flausson@normandiewebschool.fr>
2023-11-17 16:03:04 +0100

    ci cd on tow branches c1 on dev and ci cd on main

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
         <div className="hidden flex-col md:flex">
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
-            <h1>NWS rent app</h1>
+            <h1>NWS - rent app</h1>
           </div>
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
