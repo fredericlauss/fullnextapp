97d0504c83033a879eeabb504bc9d708c2a3d58e
frederic <flausson@normandiewebschool.fr>
2023-11-17 15:12:38 +0100

    change rache url

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
             <div className="flex items-center space-x-2">
-            <p className="text-sm text-muted-foreground">Conforme à la <a className="underline" target="_blank" href="https://www.la-rache.com/presentation.html">R.A.C.H.E</a></p>
+            <p className="text-sm text-muted-foreground">Conforme à la <a className="underline" target="_blank" href="https://www.la-rache.com">R.A.C.H.E</a></p>
             </div>
           </div>
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
