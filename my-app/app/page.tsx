61382de7e60838e96fc79fb79b401662abb13ab6
frederic <flausson@normandiewebschool.fr>
2023-12-06 11:57:10 +0100

    il y a de la place sur le vps

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+            <h2 className="text-3xl font-bold tracking-tight">Dashboard qui win</h2>
             <div className="flex items-center space-x-2">
             <p className="text-sm text-muted-foreground">Conforme à la <a className="underline" target="_blank" href="https://www.la-rache.com">R.A.C.H.E</a></p>
             </div>
